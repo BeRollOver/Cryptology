b4a40d4916a4e6b087311d19b4ded78c5d005abb
rollover <mr.mustard@yandex.ru>
2015-09-08 23:50:31 +0300

    Первый коммит

---
+++ Cryptology/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cryptology.Startup))]
+namespace Cryptology
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
