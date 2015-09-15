2395f045279d107703b1e685a00be0618abdb6ff
rollover <mr.mustard@yandex.ru>
2015-09-15 16:00:28 +0300

    Добавлена проверка чисел Мерсена

---
+++ Cryptology/Controllers/MersennePrimeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cryptology.Models;
+
+namespace Cryptology.Controllers
+{
+    public class MersennePrimeController : Controller
+    {
+        // GET: MersennePrimzahl
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MersennePrimeModel model)
+        {
+            return View("Echo", model);
+        }
+    }
+}
---
+++ Cryptology/Models/MersennePrimeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cryptology.Models
+{
+    public class MersennePrimeModel
+    {
+        [Range(3, 64, ErrorMessage = "Пожалуйста, введите число от 3 до 64")]
+        public int p { get; set; }
+    }
+}
